a0af469314c28adc50fcce3dca8c1c4d688690c9
xuanviet73 <2012393@dlu.edu.vn>
2023-07-03 00:38:29 +0700

    Dinh01VR

---
+++ data.js
@@ -0,0 +1,549 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cng-dinh-1",
+      "name": "Cổng Dinh 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4006798648781145
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4011754276034125,
+          "pitch": 0.1352641471389564,
+          "rotation": 0,
+          "target": "1-li-vo-dinh-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8098356638985091,
+          "pitch": -0.29035555026039717,
+          "title": "Dinh 1",
+          "text": "Dinh 1 Đà Lạt hay còn được du khách biết đến với tên gọi là Dinh Bảo Đại 1. Đà Lạt là một thành phố có khí hậu quanh năm mát mẻ, dễ chịu. Nơi đây không khác gì một thiêng đường trong mơ mà ai ai cũng ao ước được sống ở đây. Chính vì thế mà vua Bảo Đại đã chọn xây dựng nên 3 dinh thự xa hoa tráng lệ tại mảnh đất Tây Nguyên Lâm Viên này."
+        }
+      ]
+    },
+    {
+      "id": "1-li-vo-dinh-1",
+      "name": "Lối vào Dinh 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08885084106724328,
+          "pitch": 0.12901314426719068,
+          "rotation": 0,
+          "target": "2-dinh-1"
+        },
+        {
+          "yaw": -3.0950513344655697,
+          "pitch": 0.14339028006944332,
+          "rotation": 0,
+          "target": "0-cng-dinh-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dinh-1",
+      "name": "Dinh 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.021743072373133998,
+          "pitch": -0.0126550487005197,
+          "rotation": 0,
+          "target": "3-snh-chnh"
+        },
+        {
+          "yaw": -2.0307224414660165,
+          "pitch": 0.10177557521321212,
+          "rotation": 0,
+          "target": "11-sn-trc-thng"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.0023345634769302848,
+          "pitch": -0.5086078899511612,
+          "title": "Lịch sử Dinh 1",
+          "text": "Chủ nhân của căn biệt điện này đã bán cho một người pháp. Lúc ấy cha vợ của vua Bảo Đại đã hỗ trợ tài chính mua lại cho vua Bải Đại vào năm 1949. Cha vợ của vua Bải Đại chính là quận công Nguyễn Hữu Hào. Về sau vua Bảo Đại đã đặt Tống Hành Dinh tại đây và lấy nơi đây là nơi làm việc trong thời gian ông làm Quốc Trưởng từ giai đoạn 1949 đến 1954."
+        }
+      ]
+    },
+    {
+      "id": "3-snh-chnh",
+      "name": "Sảnh chính",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0757517765071647,
+          "pitch": 0.13243401794148468,
+          "rotation": 0,
+          "target": "4-phng-trng-by"
+        },
+        {
+          "yaw": -2.4190597986345406,
+          "pitch": -0.045899262617121295,
+          "rotation": 0.7853981633974483,
+          "target": "5-hnh-lang-lu-2"
+        },
+        {
+          "yaw": 2.503315531052655,
+          "pitch": 0.04695563495458366,
+          "rotation": 4.71238898038469,
+          "target": "2-dinh-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.05593898587151713,
+          "pitch": -0.2960671899456333,
+          "title": "Phòng tiếp khách",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "4-phng-trng-by",
+      "name": "Phòng trưng bày",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5284108946195332,
+          "pitch": 0.051027856596443044,
+          "rotation": 4.71238898038469,
+          "target": "3-snh-chnh"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-hnh-lang-lu-2",
+      "name": "Hành lang lầu 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5795506607321173,
+          "pitch": 0.02444856709367116,
+          "rotation": 4.71238898038469,
+          "target": "6-phng-thi-t-bo-long"
+        },
+        {
+          "yaw": -1.5306455803654373,
+          "pitch": 0.1114584835252792,
+          "rotation": 0,
+          "target": "7-phng-tng-sp"
+        },
+        {
+          "yaw": 2.5159095185430926,
+          "pitch": 0.1232805702029438,
+          "rotation": 3.141592653589793,
+          "target": "3-snh-chnh"
+        },
+        {
+          "yaw": 1.2717727408635238,
+          "pitch": 0.07322458328547654,
+          "rotation": 4.71238898038469,
+          "target": "8-phng-hp"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-phng-thi-t-bo-long",
+      "name": "Phòng thái Tử Bảo Long",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4006798648781145
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8276487282369063,
+          "pitch": -0.05252532151826017,
+          "rotation": 7.853981633974483,
+          "target": "5-hnh-lang-lu-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-phng-tng-sp",
+      "name": "Phòng tượng sáp",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1346118588084426,
+          "pitch": 0.2633713361083121,
+          "rotation": 0,
+          "target": "5-hnh-lang-lu-2"
+        },
+        {
+          "yaw": -1.8783664797006416,
+          "pitch": 0.41988705153869077,
+          "rotation": 0,
+          "target": "9-phng-vua-bo-i"
+        },
+        {
+          "yaw": 1.9055429267359774,
+          "pitch": 0.24565858521796002,
+          "rotation": 0,
+          "target": "10-phng-nam-phng-hong-hu"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-phng-hp",
+      "name": "Phòng họp",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1182761516124344,
+          "pitch": -0.038908359652349844,
+          "rotation": 0,
+          "target": "5-hnh-lang-lu-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-phng-vua-bo-i",
+      "name": "Phòng vua Bảo Đại",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8833062887653993,
+          "pitch": 0.03984114639885128,
+          "rotation": 7.853981633974483,
+          "target": "5-hnh-lang-lu-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-phng-nam-phng-hong-hu",
+      "name": "Phòng Nam Phương hoàng hậu",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1675439300444914,
+          "pitch": -0.02497242037122227,
+          "rotation": 10.995574287564278,
+          "target": "7-phng-tng-sp"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2187386964269873,
+          "pitch": -0.4517639327681522,
+          "title": "Phòng Nam Phương Hoàng Hậu",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "11-sn-trc-thng",
+      "name": "Sân trực thăng",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.792688032708652,
+          "pitch": 0.12726671566094971,
+          "rotation": 0,
+          "target": "2-dinh-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
